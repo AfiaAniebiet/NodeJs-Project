23dde964dd60a830891ceefe5bf6174258a6c0a7
aniebietafia <aniebietafia87@gmail.com>
2022-10-13 18:06:54 +0100

    Adding Dynamic Content using Pug

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const adminData = require("./routes/admin");
 const shopRoute = require("./routes/shop");
 
 const app = express();
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
--- routes/shop.js
+++ routes/shop.js
@@ -7,9 +7,15 @@ const adminData = require("./admin");
 const Router = express.Router();
 
 Router.get("/", (req, res, next) => {
-  console.log(adminData.products);
+  const products = adminData.products;
+  res.render("shop", {
+    prods: products,
+    docTitle: "Product Shop",
+    prodTitle: "Products",
+  });
+  // console.log(adminData.products);
   //res.sendFile(path.join(__dirname, "../", "views", "shop.html")); //Version 1
-  res.sendFile(path.join(rootDir, "views", "shop.html")); // Version 2
+  // res.sendFile(path.join(rootDir, "views", "shop.html")); // Version 2
 });
 
 module.exports = Router;
